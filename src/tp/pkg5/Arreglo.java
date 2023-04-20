cc7743ca36dd2ec36c2e45d9661b9533e5bbd548
jb-2015 <juan_matias_becerra@hotmail.com>
2023-04-20 19:12:21 -0300

    correccion

--- src/tp/pkg5/Arreglo.java
+++ src/tp/pkg5/Arreglo.java
@@ -10,67 +10,68 @@ package tp.pkg5;
  * @author juany
  */
 public class Arreglo {
-    
-    
-    
-    public static void sumarLista(int[] arr){
-        int suma=0;
-        for(int e : arr){
-            suma+=e;
+
+    public static void sumarLista(int[] arr) {
+        int suma = 0;
+        for (int e : arr) {
+            suma += e;
         }
-        
-        System.out.println("La suma es : "+ suma );
-        System.out.println("El promedio es : "+(suma/arr.length));
+
+        System.out.println("La suma es : " + suma);
+        System.out.println("El promedio es : " + (suma / arr.length));
     }
-    
-    public static int buscarMayor(int[][] arr){
-        int mayor= arr[0][0];
-        for(int i=0; i<arr.length;i++){
-            for(int j = 0;j < arr[i].length; j++){
-                if(arr[i][j]>mayor){
-                    mayor=arr[i][j];
+
+    public static int buscarMayor(int[][] arr) {
+        if (arr.length > 0) {
+            int mayor = arr[0][0];
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] > mayor) {
+                        mayor = arr[i][j];
+                    }
                 }
+
             }
-            
+            return mayor;
         }
-        return mayor;
+        return 0;
     }
-    
-    public static int cuentaVocal(String word){
-        
-        String[] vocales= {"a","e","i","o","u"};
-        
+
+    public static int cuentaVocal(String word) {
+
+        String[] vocales = {"a", "e", "i", "o", "u"};
+
         String[] aux = word.split("");
-        int numV=0;
-        for(String l : aux){
-            if(contiene(vocales,l)){
+        int numV = 0;
+        for (String l : aux) {
+            if (contiene(vocales, l)) {
                 numV++;
             }
         }
-        
+
         return numV;
     }
-    
-    private static boolean contiene(String[] arr, String c){
-        for(String l:arr){
-            if(l.equalsIgnoreCase(c)){
+
+    private static boolean contiene(String[] arr, String c) {
+        for (String l : arr) {
+            if (l.equalsIgnoreCase(c)) {
                 return true;
             }
         }
         return false;
     }
-    
-    public static int cuentaCaracter(char c, String word){
-        int cant=0;
-        
+
+    public static int cuentaCaracter(char c, String word) {
+        int cant = 0;
+
         String enmin = word.toLowerCase();
-        
-        for(int i = 0 ; i < enmin.length();i++){
-            if(c == enmin.charAt(i)){
+
+        for (int i = 0; i < enmin.length(); i++) {
+            if (c == enmin.charAt(i)) {
                 cant++;
             }
         }
-        
+
         return cant;
     }
 }
